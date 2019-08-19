9a194a4e4ccdeb9b676347dd6de1854995fc76a6
udayk <udaykumar1113@gmail.com>
2019-08-19 18:52:30 -0400

    Made API call using axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
-import string from './models/Search';
-import {add, multiply, ID} from './view/searchView';
+import axios from 'axios';
 
- console.log(`using imported functions! ${add(5, 3)} and ${multiply(5, 3)}. ${ID} ${string}`);
+async function getResults(query){
+    const apiKey = '05ee40ea6c1e7cb0a693efcd161a264f';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try{
+        const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`);
+        const recipes = result.data.recipes;
+        console.log(recipes);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+getResults('pizza');
