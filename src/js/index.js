b28140f31a2ae65f1019a9e357b3ea0a18476cbf
udayk <udaykumar1113@gmail.com>
2019-08-21 08:49:31 -0400

    searched for a recipie in controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,34 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-search.getResults();
+/**Global state of the app
+ * Search object
+ * Current recipie of object
+ * Shopping list object
+ * Liked recipies
+ */
+
+ const state = {};
+
+ const controlSearch = async () => {
+
+    //1. Get uery from view
+    const query = 'pizza';
+
+    if(query){
+        //2. New search object and add to state
+        state.search = new Search(query);
+
+        //3. prepare UI for results
+
+        //4. Search for recipies
+        await state.search.getResults();
+
+        //5. Render results on UI
+        console.log(state.search.result);
+    }
+ }
+
+ document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+ }) 
